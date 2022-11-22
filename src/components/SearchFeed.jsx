34c082897f863a74ec682b9750da7fb1c30cdf8f
ferdek007 <kiepskif330@gmail.com>
2022-11-22 19:49:11 +0100

    SearchFeed component

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchSearched } from "../api/api";
+import GifsGrid from "./GifsGrid";
+
 const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+  const [gifs, setGifs] = useState([]);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    fetchSearched(searchTerm).then((data) => setGifs(data.data));
+  }, [searchTerm]);
+
+  console.log(`'${searchTerm}' search gifs: `, gifs);
+
+  return (
+    <div className="searchfeed-container">
+      <h2 className="results">
+        Search results for
+        <span style={{ color: "#F31503" }}>
+          {" " + searchTerm.replace("-", " ")}
+        </span>
+      </h2>
+      <div className="searched-container">
+        <GifsGrid gifs={gifs} />
+      </div>
+    </div>
+  );
 };
 
 export default SearchFeed;
